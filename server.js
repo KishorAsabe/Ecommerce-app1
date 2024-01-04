dbebb8cc8a1be2502ed2287a98077efc73b427b8
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-04 09:42:42 +0530

    -

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
-//import { fileURLToPath } from "url";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
 
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //database config
 connectDB();
@@ -27,7 +27,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-//app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //rotes
 app.use("/api/v1/auth", authRoutes);
@@ -35,9 +35,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-/*app.use("*", function (req, res) {
+app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});*/
+});
 
 //port
 const PORT = process.env.PORT || 8080;
